617e212e12a4f3777a6c640a931eeea14829b166
hexueying <549589502@qq.com>
2025-03-19 10:05:21 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,147 @@
-# electronNull
-electron空项目
+<!--
+ * @Description: 创建项目，打开项目
+ * @Author: hexueying
+ * @Date: 2025-03-17 16:55:26
+ * @LastEditors: hexueying
+ * @LastEditTime: 2025-03-18 14:29:38
+ * @FilePath: README.md
+-->
+
+node版本18.20.7
+#1.npm create vite@latest（创建vite项目）
+#2.npm install
+#3.npm run dev
+#4.npm install electron -D（安装electron）
+#5.新建electron文件夹=》main.js
+const { app, BrowserWindow } = require('electron')
+// const path = require("path")
+ 
+const createWindow = () => {
+  const mainWin = new BrowserWindow({
+    width: 1000,
+    height: 800,
+  })
+  
+  //将链接修改成我们运行Vue时的地址：http://localhost:5173
+  //mainWin.loadFile(path.join(__dirname, "./index.html"));
+  mainWin.loadURL('http://localhost:5173')
+ 
+}
+ 
+// 准备完成调用创建方法
+app.whenReady().then(() => {
+  createWindow()
+})
+ 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+#6.修改package.json：
+{
+  "type": "commonjs",
+  "main": "electron/main.js",
+  "scripts": {
+    "electron:dev": "electron ."
+  }
+}
+#7.electron-》新建index.html:
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      http-equiv="Content-Security-Policy"
+      content="default-src 'self'; script-src 'self'"
+    />
+    <meta
+      http-equiv="X-Content-Security-Policy"
+      content="default-src 'self'; script-src 'self'"
+    />
+    <title>Electron</title>
+  </head>
+  <body>
+    <h1>Hello Electron </h1>
+  </body>
+</html>
+
+## npm run dev
+## npm run electron:dev
+
+#npm install nodemon -D(热更新工具)
+package.json 的 scripts 标签新增一条启动命令，监控文件改变
+"electron": "nodemon --exec electron . --watch ./ --ext .js,.html,.css,.vue",
+
+#npm install npm-run-all -D（命令合并工具）
+package.json 的 scripts 标签新增，nmp run start就会同时运行 vue 和 electron
+"start": "npm-run-all --parallel dev electron",
+
+#npm install electron-builder -D(打包工具)
+package.json 的 scripts 标签新增：
+"electron:build": "vite build && electron-builder"，
+package.json 新增：
+"build": {
+    "productName": "electron-vue",
+    "appId": "electron-vue",
+    "asar": true,
+    "directories": {
+        "output": "dist_electron/${version}"
+    },
+    "files": [
+        "dist",
+        "electron"
+    ],
+    "nsis": {
+        "oneClick": false,
+        "allowToChangeInstallationDirectory": true
+    },
+    "mac": {
+        "category": "your.app.category.type"
+    },
+    "win": {
+        "icon": "./electron/log.ico",
+        "target": [
+        {
+            "target": "nsis",
+            "arch": [
+            "ia32"
+            ]
+        }
+        ]
+    },
+    "linux": {}
+}
+根据环境显示不同的地址：
+在main.js：
+const path = require("path");
+//将链接修改成我们运行Vue时的地址：http://localhost:5173
+//mainWin.loadFile(path.join(__dirname, "./index.html"));
+//mainWin.loadURL('http://localhost:5173')
+
+const env = app.isPackaged ? "production" : "development";
+const indexHtml = {
+development: "http://localhost:5173", // 开发环境
+production: path.join(__dirname, "../dist/index.html"), // 生产环境
+};
+
+mainWin.loadURL(indexHtml[env]);
+
+让vite打包后的静态资源能够以本地file协议访问
+在vite.config.js:
+mport { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {resolve} from 'path'
+ 
+export default defineConfig({
+  plugins: [vue()],
+  base:'./',
+  manifest:true,  
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  optimizeDeps: {
+    exclude: ['electron'], 
+  }
+
+#npm run electron:build(打包)
---
+++ electron/main.js
@@ -0,0 +1,185 @@
+/*
+ * @Description: 
+ * @Author: hexueying
+ * @Date: 2025-03-17 17:26:31
+ * @LastEditors: hexueying
+ * @LastEditTime: 2025-03-18 16:49:12
+ * @FilePath: main.js
+ */
+const { app, BrowserWindow,Menu  } = require('electron')
+const path = require("path")
+ 
+const createWindow = () => {
+  const mainWin = new BrowserWindow({
+    width: 1500,
+    height: 1000,
+  })
+  
+  //将链接修改成我们运行Vue时的地址：http://localhost:5173
+  //mainWin.loadFile(path.join(__dirname, "./index.html"));
+  //mainWin.loadURL('http://localhost:5173')
+ 
+  const env = app.isPackaged ? "production" : "development";
+  const indexHtml = {
+    // development: "http://localhost:5173", // 开发环境
+    development: "http://localhost:3100", // 开发环境
+    production: path.join(__dirname, "../dist/index.html"), // 生产环境
+  };
+ 
+  mainWin.loadURL(indexHtml[env]);
+}
+ 
+// 准备完成调用创建方法
+app.whenReady().then(() => {
+  createWindow()
+  createMenu();
+})
+ 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+
+function createMenu() {
+  let template = [
+    {
+      label: '文件',
+      submenu: [
+        {label: '打开',
+        accelerator: 'Ctrl+O',
+        click(){
+          console.log("open")
+        }
+        }
+      ]
+    },
+    {
+    label: '编辑',
+    submenu: [{
+      label: '撤销',
+      accelerator: 'CmdOrCtrl+Z',
+      role: 'undo'
+    }, {
+      label: '重做',
+      accelerator: 'Shift+CmdOrCtrl+Z',
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      label: '剪切',
+      accelerator: 'CmdOrCtrl+X',
+      role: 'cut'
+    }, {
+      label: '复制',
+      accelerator: 'CmdOrCtrl+C',
+      role: 'copy'
+    }, {
+      label: '粘贴',
+      accelerator: 'CmdOrCtrl+V',
+      role: 'paste'
+    }, {
+      label: '全选',
+      accelerator: 'CmdOrCtrl+A',
+      role: 'selectall'
+    }]
+  }, {
+    label: '查看',
+    submenu: [{
+      label: '重载',
+      accelerator: 'CmdOrCtrl+R',
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          // 重载之后, 刷新并关闭所有的次要窗体
+          if (focusedWindow.id === 1) {
+            BrowserWindow.getAllWindows().forEach(function (win) {
+              if (win.id > 1) {
+                win.close()
+              }
+            })
+          }
+          focusedWindow.reload()
+        }
+      }
+    }, {
+      label: '切换全屏',
+      accelerator: (function () {
+        if (process.platform === 'darwin') {
+          return 'Ctrl+Command+F'
+        } else {
+          return 'F11'
+        }
+      })(),
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+        }
+      }
+    }, {
+      label: '切换开发者工具',
+      accelerator: (function () {
+        if (process.platform === 'darwin') {
+          return 'Alt+Command+I'
+        } else {
+          return 'Ctrl+Shift+I'
+        }
+      })(),
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: '应用程序菜单演示',
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          const options = {
+            type: 'info',
+            title: '应用程序菜单演示',
+            buttons: ['好的'],
+            message: '此演示用于 "菜单" 部分, 展示如何在应用程序菜单中创建可点击的菜单项.'
+          }
+          dialog.showMessageBox(focusedWindow, options, function () {})
+        }
+      }
+    }]
+  }, {
+    label: '窗口',
+    role: 'window',
+    submenu: [{
+      label: '最小化',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    }, {
+      label: '关闭',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    }, {
+      type: 'separator'
+    }, {
+      label: '重新打开窗口',
+      accelerator: 'CmdOrCtrl+Shift+T',
+      enabled: false,
+      key: 'reopenMenuItem',
+      click: function () {
+        app.emit('activate')
+      }
+    }]
+  }, {
+    label: '帮助',
+    role: 'help',
+    submenu: [{
+      label: '学习更多',
+      click: function () {
+        shell.openExternal('http://electron.atom.io')
+      }
+    }]
+  }]
+  
+ 
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+ 
+// app.whenReady().then(createMenu);
